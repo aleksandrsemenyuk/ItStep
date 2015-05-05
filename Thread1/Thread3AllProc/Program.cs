7d9b96177a3f4c401ef46963dd5aa300686b5020
aleksandrsemenyuk <aleksandr.semenyuk@gmail.com>
2015-05-05 13:47:36 +0300

    Add 1 claswork

---
+++ Thread1/Thread3AllProc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Thread3AllProc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] process = Process.GetProcesses();
+                  foreach (Process p in process)
+             {
+                 Console.WriteLine("{0}   - {1}", p.ProcessName, p.Id);
+             }
+                  foreach (Process p in process)
+                  {
+                      if (p.Id == 1184 || p.Id == 2828)
+                          p.Close();
+                  }
+        }
+
+  
+    }
+}
---
+++ Thread1/ThreadManip/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ThreadManip
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Process myProcess = new Process();
+        public MainWindow()
+        {
+            InitializeComponent();
+            myProcess.StartInfo.FileName="calc.exe";
+         
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            myProcess.Start();
+            tbInfo.Text = Environment.NewLine;
+            tbInfo.Text = Environment.NewLine;
+            tbInfo.Text = "Запущений процес";
+            tbInfo.Text = Environment.NewLine;
+            myProcess.WaitForExit();
+            tbInfo.Text = "Завешений процес";
+        }
+    }
+}
